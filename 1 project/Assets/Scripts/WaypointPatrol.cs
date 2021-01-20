ebc0fcc70d17d7e479c9209e0bec4b79e18d42c8
Otrepiev <75855703+Otrepiev@users.noreply.github.com>
2021-01-20 10:49:52 +0400

    424

---
+++ 1 project/Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,108 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPatrol : MonoBehaviour
+{
+    [SerializeField] private string _playerTag = "Player";
+
+    [SerializeField] private float _distanceForPursuit = 3.5f;
+
+    private float DistanceForPursuitSQR => _distanceForPursuit * _distanceForPursuit;
+
+    public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+
+    private int m_CurrentWaypointIndex;
+
+    private GameObject _player;
+
+    private bool _isInPursuit = false;
+
+    private Vector3 _lastDestinationBeforePursuit;
+
+
+
+    private void Awake()
+    {
+        _player = GameObject.FindGameObjectWithTag(_playerTag);
+    }
+
+    private void Start ()
+    {
+        navMeshAgent.SetDestination(waypoints[0].position);
+    }
+
+    private void Update ()
+    {
+        if(IsPlayerTooClose() && !IsHaveObstacleBetween())
+        {
+            _lastDestinationBeforePursuit = navMeshAgent.destination;
+            _isInPursuit = true;
+        }
+        else
+        {
+            _lastDestinationBeforePursuit = Vector3.zero;
+            _isInPursuit = false;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (_isInPursuit)
+        {
+            navMeshAgent.SetDestination(_player.transform.position);
+        }
+        else
+        {
+            var isCurrectDistantionInWaypoints =
+                (from t in waypoints
+                 where t.position == navMeshAgent.destination
+                 select t.position).Count() == 1;
+
+            if (!isCurrectDistantionInWaypoints)
+            {
+                navMeshAgent.SetDestination(_lastDestinationBeforePursuit);
+            }
+            else
+                if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+            {
+                m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
+                navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+            }
+        }
+    }
+
+        private bool IsHaveObstacleBetween()
+        {
+            var hitDirection = (_player.tranform.position - transform.position).normalized;
+
+            var realDistance = _distanceForPursuit;
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, hitDirection, out hit, _distanceForPursuit))
+            {
+                realDistance = hit.distance;
+            }
+
+            Debug.DrawRay(transform.position, hitDirection * realDistance, Color.red);
+
+            if (hit.transform.tag == _playerTag) return false;
+
+            return true;
+        }
+
+        private bool IsPlayerTooClose()
+        {
+            var distanceFromBotToPlayerSQR = (_player.transform.position - transform.position).sqrMagnitude;
+
+            if (distanceFromBotToPlayerSQR > DistanceForPursuitSQR)
+                return false;
+
+            return true;
+        }
+}
